046aff4f2ca121b763ecb82889faadcbf84d08b2
joshkim2441 <joshkim2441@gmail.com>
2024-04-30 00:32:50 +0300

    AirBnB v3

--- models/base_model.py
+++ models/base_model.py
@@ -26,9 +26,9 @@ class BaseModel:
     if STORAGE_TYPE == "db":
         id = Column(String(60), nullable=False, primary_key=True)
         created_at = Column(DateTime, nullable=False,
-                             default=datetime.utcnow)
+                            default=datetime.utcnow)
         updated_at = Column(DateTime, nullable=False,
-                             default=datetime.utcnow)
+                            default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
@@ -37,6 +37,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at()
 
     def __set_attributes(self, attr_dict):
         """Converts attr_dict values to python attributes"""
@@ -59,28 +60,28 @@ class BaseModel:
         for attr, val in attr_dict.items():
             setattr(self, attr, val)
 
-        def __is_serializable(self, obj_v):
-            """Checks if object is serializable: private """
-            try:
-                obj_to_string = json.dumps(obj_v)
-                return obj_to_string is not None and isinstance(
-                    obj_to_string, str)
-            except Exception:
-                return False
+    def __is_serializable(self, obj_v):
+        """Checks if object is serializable: private """
+        try:
+            obj_to_string = json.dumps(obj_v)
+            return obj_to_string is not None and isinstance(
+                obj_to_string, str)
+        except Exception:
+            return False
 
-        def bm_update(self, attr_dict=None):
-            """Updates basemodel, sets correct attributes"""
-            IGNORE = [
-                'id', 'created_at', 'updated_at', 'email',
-                'state_id', 'user_id', 'city_id', 'place_id'
-            ]
-            if attr_dict:
-                updated_dict = {
-                    k: v for k, v in attr_dict.items() if k not in IGNORE
-                }
-                for key, value in updated_dict.items():
-                    setattr(self, key, value)
-                self.save()
+    def bm_update(self, attr_dict=None):
+        """Updates basemodel, sets correct attributes"""
+        IGNORE = [
+            'id', 'created_at', 'updated_at', 'email',
+            'state_id', 'user_id', 'city_id', 'place_id'
+        ]
+        if attr_dict:
+            updated_dict = {
+                k: v for k, v in attr_dict.items() if k not in IGNORE
+            }
+            for key, value in updated_dict.items():
+                setattr(self, key, value)
+            self.save()
 
     def __str__(self):
         """String representation of the BaseModel class"""
