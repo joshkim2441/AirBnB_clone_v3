acfcfa51327495da889c8f1b996f2b0d8acb6169
joshkim2441 <joshkim2441@gmail.com>
2024-04-30 00:36:53 +0300

    api

--- models/base_model.py
+++ models/base_model.py
@@ -33,11 +33,23 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
         if kwargs:
-            self.__set_attributes(kwargs)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = self.created_at()
+            self.updated_at = self.created_at
 
     def __set_attributes(self, attr_dict):
         """Converts attr_dict values to python attributes"""
