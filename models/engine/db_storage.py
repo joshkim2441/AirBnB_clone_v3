5748b54902e7117af8daa64f2714ace15215c906
joshkim2441 <joshkim2441@gmail.com>
2024-04-29 07:45:46 +0300

    AirBnB v3

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -58,20 +58,15 @@ class DBStorage:
         """
            returns a dictionary of all objects
         """
-        obj_dict = {}
-        if cls is not None:
-            a_query = self.__session.query(DBStorage.CNC[cls])
-            for obj in a_query:
-                obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
-                obj_dict[obj_ref] = obj
-            return obj_dict
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
-        for c in DBStorage.CNC.values():
-            a_query = self.__session.query(c)
-            for obj in a_query:
-                obj_ref = "{}.{}".format(type(obj).__name__, obj.id)
-                obj_dict[obj_ref] = obj
-        return obj_dict
 
     def new(self, obj):
         """add the object to the current database session"""
