19865a6a55593654a1a51cd7fe974ac7a463d744
Elizabeth <mahlatsemostsinone@gmail.com>
2024-04-28 22:37:26 -0700

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,9 +8,9 @@ from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 app.url_map.strict_slashes = False
-app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
