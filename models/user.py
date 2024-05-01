d19f8fea411f82f68d347295186a73b75a7888d2
Elizabeth <mahlatsemostsinone@gmail.com>
2024-05-01 10:20:11 -0700

    AirBnB Clone v3

--- models/user.py
+++ models/user.py
@@ -31,9 +31,8 @@ class User(BaseModel, Base):
         """initializes user"""
         super().__init__(*args, **kwargs)
 
-    def __set_password(self, pwd):
-        """ Custom setter to encrypt password using MD5"""
-        secure = hashlib.md5()
-        secure.update(pwd.encode("utf-8"))
-        secure_password = secure.hexdigest()
-        setattr(self, "password", secure_password)
+    def __setattr__(self, name, value):
+        """sets a password with md5 encryption"""
+    if name == "password":
+            value = md5(value.encode()).hexdigest()
+        super().__setattr__(name, value)
