d2ef7ac23ea258d5e7f244195407af2dca4c2aea
Elizabeth <mahlatsemostsinone@gmail.com>
2024-05-01 15:59:50 -0700

    api

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """ objects that handles all default RestFul API actions for cities """
+from models.amenity import Amenity
+from models.base_model import BaseModel
 from models.city import City
+from models.place import Place
+from models.review import Review
 from models.state import State
+from models.user import User
 from models import storage
 from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
